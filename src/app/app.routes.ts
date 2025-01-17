f71ce2a917b1927c7c69c1958d71010e0f52baf2
Likeur <jacksimba28@gmail.com>
2025-01-18 00:51:09 +0200

    initialization and tailwind setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        title: 'win 12 home',
+        loadComponent: () => import('./layout/home/home.component')
+    }
+];
---
+++ src/app/components/taskbar/taskbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taskbar',
+  imports: [],
+  templateUrl: './taskbar.component.html',
+  styles: ``
+})
+export class TaskbarComponent {
+
+}
