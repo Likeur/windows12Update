9b462719fcb23c28d5909b65a92903ae0da43845
Likeur <jacksimba28@gmail.com>
2025-01-18 22:42:05 +0200

    adding setting links

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ILink } from '../../interfaces/link.interface';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './settings.component.html',
   styles: ``
 })
 export class SettingsComponent {
+  public linkstyle = signal('p-2 flex rounded-md items-center gap-2 hover:bg-white/10 duration-200 w-full')
 
+  public linkList : ILink[] =  [
+    {
+      name: 'Home',
+      linkroute: 'home'
+    },
+    {
+      name: 'System',
+      linkroute: 'home'
+    },
+    {
+      name: 'Bluetooth & devices',
+      linkroute: 'home'
+    },
+    {
+      name: 'Network',
+      linkroute: 'home'
+    },
+    {
+      name: 'Apps',
+      linkroute: 'home'
+    },
+    {
+      name: 'Accounts',
+      linkroute: 'home'
+    },
+    {
+      name: 'Time & language',
+      linkroute: 'home'
+    },
+    {
+      name: 'Gaming',
+      linkroute: 'home'
+    },
+    {
+      name: 'Accessibility',
+      linkroute: 'home'
+    },
+    {
+      name: 'Privacy & security',
+      linkroute: 'home'
+    },
+    {
+      name: 'Windows Update',
+      linkroute: 'home'
+    },
+  ]
 }
