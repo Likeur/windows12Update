5534edae5d246d71bda075c2b9fe050d772f5052
Likeur <jacksimba28@gmail.com>
2025-01-18 23:51:27 +0200

    adding vidéo player

---
+++ src/app/components/videoplayer/videoplayer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-videoplayer',
+  imports: [],
+  templateUrl: './videoplayer.component.html',
+  styles: ``
+})
+export class VideoplayerComponent {
+
+}
--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { TaskbarComponent } from '../../components/taskbar/taskbar.component';
 import { SettingsComponent } from '../../components/settings/settings.component';
+import { CommonModule } from '@angular/common';
+import { VideoplayerComponent } from '../../components/videoplayer/videoplayer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TaskbarComponent, SettingsComponent],
+  imports: [TaskbarComponent, SettingsComponent, VideoplayerComponent,CommonModule],
   templateUrl: './home.component.html',
   styles: ``
 })
 export default class HomeComponent {
+
+  public isInFront = false
+
+  public bringToFront(){
+    this.isInFront = !this.isInFront
+  }
+
 
 }
