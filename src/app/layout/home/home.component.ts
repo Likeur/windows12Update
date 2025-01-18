a0f9f5ffe61a02653c60fa2928f5c42b7a6f62d5
Likeur <jacksimba28@gmail.com>
2025-01-18 21:47:34 +0200

    adding setting parts

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TaskbarComponent } from '../../components/taskbar/taskbar.component';
+import { SettingsComponent } from '../../components/settings/settings.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TaskbarComponent],
+  imports: [TaskbarComponent, SettingsComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
