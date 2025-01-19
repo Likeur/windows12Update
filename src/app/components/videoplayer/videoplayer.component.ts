7d2f01f37e61351af6471b30e6d581a81ad2dcb5
Likeur <jacksimba28@gmail.com>
2025-01-19 11:49:04 +0200

    adding controls to the video player

--- src/app/components/videoplayer/videoplayer.component.ts
+++ src/app/components/videoplayer/videoplayer.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { IVideo } from '../../interfaces/vid.interface';
 
 @Component({
   selector: 'app-videoplayer',
@@ -8,7 +9,13 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class VideoplayerComponent {
 
-  videoIsplayed = false
+  public videosAvailable : IVideo[] = [
+    {name:"video 1", linktovideo:"/videos/vidtest.mp4"},
+    {name:"video 2", linktovideo:"/videos/vidtest2.mp4"},
+    {name:"video 3", linktovideo:"/videos/vidtest3.mp4"}
+  ]
+
+  public videoIsplayed = false
 
   @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
@@ -19,5 +26,11 @@ export class VideoplayerComponent {
     }else{
       this.videoPlayer.nativeElement.pause();
     }
+  }
+
+  public currentVideo: string = this.videosAvailable[0].linktovideo; // Vidéo par défaut
+
+  public changeVideo(videoUrl: string) {
+    this.currentVideo = videoUrl;
   }
 }
